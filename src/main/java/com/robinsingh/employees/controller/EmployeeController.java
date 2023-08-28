f87fef276a736619f3d0434478c203603c7f390e
Robin SIngh <robin.122497@gmail.com>
2023-08-29 01:11:39 +0530

    Added Logic to save employee

--- src/main/java/com/robinsingh/employees/controller/EmployeeController.java
+++ src/main/java/com/robinsingh/employees/controller/EmployeeController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -41,7 +43,13 @@ public class EmployeeController {
 
 		Employee employee = new Employee();
 		theModel.addAttribute(employee);
-		return "employees/employee-form.html";
+		return "employees/employee-form";
+	}
+
+	@PostMapping("/save")
+	public String save(@ModelAttribute("employee") Employee theEmployee){
+		employeeService.save(theEmployee);
+		return "redirect:/employees/list";
 	}
 }
 
