70b5e259292ec19c26e6161a711ad90e901e7670
Robin SIngh <robin.122497@gmail.com>
2023-08-28 14:04:13 +0530

    Initial commit

---
+++ src/main/java/com/robinsingh/employees/controller/EmployeeController.java
@@ -0,0 +1,47 @@
+package com.robinsingh.employees.controller;
+
+import com.robinsingh.employees.entity.Employee;
+import com.robinsingh.employees.service.EmployeeService;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+	private EmployeeService employeeService;
+
+	@Autowired
+	public EmployeeController(EmployeeService theEmployeeService){
+		employeeService = theEmployeeService;
+	}
+
+	// add mapping for "/list"
+
+	@GetMapping("/list")
+	public String listEmployees(Model theModel) {
+
+		// load employee data
+		List<Employee> theEmployees = employeeService.findAll();
+		// add to the spring model
+		theModel.addAttribute("employees", theEmployees);
+
+		return "list-employees";
+	}
+}
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/robinsingh/employees/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.robinsingh.employees.service;
+
+import java.util.List;
+
+import com.robinsingh.employees.entity.Employee;
+
+public interface EmployeeService {
+
+	List<Employee> findAll();
+	
+	Employee findById(int theId);
+	
+	void save(Employee theEmployee);
+	
+	void deleteById(int theId);
+	
+}
